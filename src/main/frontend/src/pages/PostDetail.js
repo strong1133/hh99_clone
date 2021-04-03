11581686b3a20c5757f9dcbfcd19d19e73b3801a
ouo_yoonk <joyk9251@gmail.com>
2021-04-03 14:52:16 +0900

    수정중

--- src/main/frontend/src/pages/PostDetail.js
+++ src/main/frontend/src/pages/PostDetail.js
@@ -6,7 +6,17 @@ import { Grid, Text, Image, Button } from '../elements';
 const PostDetail = (props) => {
   return (
     <React.Fragment>
-      <div></div>
+      <div className="header">
+        <div>웹 개발 작업을 더 쉽고 효과적으로 마들어주는 유용한 도구들!</div>
+        <div>
+          <b>openhub</b>· 2021년 3월 13일{' '}
+        </div>
+        <div>하트 좋아요 개수</div>
+        <div>해시태그 컴포넌트</div>
+      </div>
+
+      <div>프로젝트박스</div>
+      <div>Contents</div>
     </React.Fragment>
   );
 };
