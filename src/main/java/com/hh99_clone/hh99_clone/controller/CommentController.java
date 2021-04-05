3a8ca788c2767ef3c2a61ecc511b1903ff12f740
conagreen <icoqwee@gmail.com>
2021-04-05 22:43:18 +0900

    cona 브랜치 삭제를 위한  커밋

--- src/main/java/com/hh99_clone/hh99_clone/controller/CommentController.java
+++ src/main/java/com/hh99_clone/hh99_clone/controller/CommentController.java
@@ -3,39 +3,38 @@ package com.hh99_clone.hh99_clone.controller;
 import com.hh99_clone.hh99_clone.domain.Comment;
 import com.hh99_clone.hh99_clone.dto.CommentRequestDto;
 import com.hh99_clone.hh99_clone.service.CommentService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @RestController
 public class CommentController {
     private final CommentService commentService;
 
-    @Autowired
-    public CommentController(CommentService commentService){
-        this.commentService = commentService;
-    }
-
-    // 댓글 등록
-    @PostMapping ("/api/comments")
-    public Comment createComment(@RequestBody CommentRequestDto commentRequestDto){
-        Comment comment = commentService.createComment(commentRequestDto);
-        return comment;
-    }
+//    @Autowired
+//    public CommentController(CommentService commentService){
+//        this.commentService = commentService;
+//    }
 
     // 댓글 조회
     @GetMapping("/api/comments")
     public List<Comment> getComments(){
-
         return commentService.getComments();
+    }
+
+    // 댓글 작성
+    @PostMapping ("/api/comments")
+    public Comment createComment(@RequestBody CommentRequestDto commentRequestDto){
+        return commentService.createComment(commentRequestDto);
     }
 
     // 댓글 수정
     @PutMapping("/api/comments/{id}")
     public Long updateComment(@PathVariable Long id, @RequestBody CommentRequestDto commentRequestDto){
-        Comment comment = commentService.updateComment(id, commentRequestDto);
-        return comment.getId();
+        return commentService.updateComment(id, commentRequestDto);
     }
 
     // 댓글 삭제
--- src/main/java/com/hh99_clone/hh99_clone/service/CommentService.java
+++ src/main/java/com/hh99_clone/hh99_clone/service/CommentService.java
@@ -3,6 +3,7 @@ package com.hh99_clone.hh99_clone.service;
 import com.hh99_clone.hh99_clone.domain.Comment;
 import com.hh99_clone.hh99_clone.dto.CommentRequestDto;
 import com.hh99_clone.hh99_clone.repository.CommentRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,40 +11,42 @@ import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class CommentService {
 
-    @Autowired
     private final CommentRepository commentRepository;
 
-    public CommentService(CommentRepository commentRepository){
-        this.commentRepository = commentRepository;
-    }
-
-    // 댓글 등록
-    @Transactional
-    public Comment createComment(CommentRequestDto commentRequestDto) {
-        Comment comment = new Comment(commentRequestDto);
-        commentRepository.save(comment);
-        
-        return comment;
-    }
+//    @Autowired
+//    public CommentService(CommentRepository commentRepository){
+//        this.commentRepository = commentRepository;
+//    }
 
     // 댓글 조회
+    @Transactional
     public List<Comment> getComments() {
         return commentRepository.findAll();
     }
 
+    // 댓글 작성
+    @Transactional
+    public Comment createComment(CommentRequestDto commentRequestDto) {
+        Comment comment = new Comment(commentRequestDto);
+        commentRepository.save(comment);
+        return comment;
+    }
+
     // 댓글 수정
     @Transactional
-    public Comment updateComment(Long id, CommentRequestDto commentRequestDto) {
+    public Long updateComment(Long id, CommentRequestDto commentRequestDto) {
         Comment comment = commentRepository.findById(id).orElseThrow(
                 () -> new NullPointerException("해당 아이디가 존재하지 않습니다.")
         );
         comment.update(commentRequestDto);
-        return comment;
+        return comment.getId();
     }
 
     // 댓글 삭제
+    @Transactional
     public Long deleteComment(Long id) {
         commentRepository.deleteById(id);
         return id;
