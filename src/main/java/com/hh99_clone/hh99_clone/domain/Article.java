140e4e6aaff1280f47a87c8405fa58ae57e2feee
[strong1133] <[strong1133@naver.com]>
2021-04-07 20:48:17 +0900

    add column

--- src/main/java/com/hh99_clone/hh99_clone/domain/Article.java
+++ src/main/java/com/hh99_clone/hh99_clone/domain/Article.java
@@ -17,21 +17,32 @@ public class  Article extends Timestamped {
     private Long id;
 
     @Column(nullable = false)
-    private String title; // 게시글 제목
+    private String title;           // 게시글 제목
+
+    @Column(length = 1000, nullable = false)
+    private String contents;        // 게시글 텍스트형태 (메인 미리보기용)
+
+    @Column(length = 1000, nullable = false)
+    private String contentsHtml;    // 게시글 Html형태
+
+    @Column(length = 1000, nullable = false)
+    private String contentsMd;      // 게시글 MarkDown 형태
 
     @Column(nullable = false)
-    private String contents; // 게시글 내용
+    private String image;           // 게시글 사진링크
+
     @Column(nullable = false)
-    private String image; // 게시글 사진링크
+    private String author;          // 게시글 작성자
+
     @Column(nullable = false)
-    private String author; // 게시글 작성자
-    @Column(nullable = false)
-    private int liked; // 게시글 좋아요 갯수
+    private int liked;              // 게시글 좋아요 갯수
 
     // Article 더미 데이터 생성자
-    public Article (String title, String contents, String image, String author, int liked){
+    public Article (String title, String contents, String contentsHtml, String contentsMd,String image, String author, int liked){
         this.title = title;
         this.contents = contents;
+        this.contentsHtml = contentsHtml;
+        this.contentsMd = contentsMd;
         this.image = image;
         this.author = author;
         this.liked = liked;
@@ -41,6 +52,8 @@ public class  Article extends Timestamped {
     public Article (ArticleRequestDto articleRequestDto){
         this.title = articleRequestDto.getTitle();
         this.contents = articleRequestDto.getContents();
+        this.contentsHtml = articleRequestDto.getContentsHtml();
+        this.contentsMd = articleRequestDto.getContentsMd();
         this.image = articleRequestDto.getImage();
         this.author = articleRequestDto.getAuthor();
         this.liked = 0;
@@ -50,6 +63,8 @@ public class  Article extends Timestamped {
     public void update (ArticleRequestDto articleRequestDto){
         this.title = articleRequestDto.getTitle();
         this.contents = articleRequestDto.getContents();
+        this.contentsHtml = articleRequestDto.getContentsHtml();
+        this.contentsMd = articleRequestDto.getContentsMd();
         this.image = articleRequestDto.getImage();
         this.author = articleRequestDto.getAuthor();
         this.liked = articleRequestDto.getLiked();
