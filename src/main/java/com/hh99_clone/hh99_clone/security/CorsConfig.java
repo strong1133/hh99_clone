91baba1cc42bddbcfc01e259e59627aacfbb186c
[strong1133] <[strong1133@naver.com]>
2021-04-07 19:36:08 +0900

    cors fixed

--- src/main/java/com/hh99_clone/hh99_clone/security/CorsConfig.java
+++ src/main/java/com/hh99_clone/hh99_clone/security/CorsConfig.java
@@ -2,19 +2,22 @@ package com.hh99_clone.hh99_clone.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
 public class CorsConfig {
+
 
    @Bean
    public CorsFilter corsFilter() {
       UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
       CorsConfiguration config = new CorsConfiguration();
       config.setAllowCredentials(true);
-      config.addAllowedOrigin("*");
+      config.addAllowedOrigin("http://localhost:3000");
       config.addAllowedHeader("*");
       config.addAllowedMethod("*");
 
